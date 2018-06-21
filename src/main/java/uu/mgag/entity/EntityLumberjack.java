01b944673410c77ef06f04ae85f5990c57fec132
Dimas Tichelaar <dimas.tichelaar@gmail.com>
2018-06-21 18:07:18 +0200

    Lumberjack now works with new task system, ChopWood now uses EntityAIMoveToBlock

--- src/main/java/uu/mgag/entity/EntityLumberjack.java
+++ src/main/java/uu/mgag/entity/EntityLumberjack.java
@@ -1,15 +1,14 @@
 package uu.mgag.entity;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.INpc;
 import net.minecraft.init.Blocks;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
-import uu.mgag.entity.ai.EntityAIAccessChest;
-import uu.mgag.entity.ai.EntityAIMineResource;
-import uu.mgag.entity.ai.EntityAIMoveToResource;
-import uu.mgag.entity.ai.EntityAIMoveToSupplyPoint;
+import uu.mgag.entity.ai.*;
+import uu.mgag.util.enums.EnumEntityStage;
 import uu.mgag.util.enums.EnumSupplyOffset;
 
 public class EntityLumberjack extends EntityWorker implements INpc
@@ -17,7 +16,8 @@ public class EntityLumberjack extends EntityWorker implements INpc
 	private EntityAIMoveToSupplyPoint moveToSupplyPoint = new EntityAIMoveToSupplyPoint(this, 0.6D, EnumSupplyOffset.BUILDING_MATERIALS);
 	private EntityAIAccessChest accessChest = new EntityAIAccessChest(this, 0.6D, Block.getIdFromBlock(Blocks.LOG), 1, true);
 	private EntityAIMineResource mineResource = new EntityAIMineResource(this, 0.6D, Blocks.LOG);
-	private EntityAIMoveToResource moveToResource = new EntityAIMoveToResource(this, 0.6D, Blocks.LOG);
+    private EntityAIChopWood chopWood = new EntityAIChopWood(this, 0.6D, Blocks.LOG);
+    private EntityAIMoveToResource moveToResource = new EntityAIMoveToResource(this, 0.6D, Blocks.LOG);
 	
 	// STAGES: 0 = movetoresource, 1 = mine, 2 = movetosupply, 3 = deposit
 	
@@ -39,34 +39,40 @@ public class EntityLumberjack extends EntityWorker implements INpc
         {
             this.areAdditionalTasksSet = true; 
             
-            this.tasks.addTask(2, moveToResource);
+            //this.tasks.addTask(2, moveToResource);
             this.tasks.addTask(2, moveToSupplyPoint);
-            this.tasks.addTask(2, accessChest);
-            this.tasks.addTask(2, mineResource);
+            //this.tasks.addTask(2, accessChest);
+            this.tasks.addTask(2, chopWood);
         }		
     }
 	
 	protected void updateAITasks()
 	{
-		/*
-		if (stage < 0 || stage > 3) stage = 0;
-		
-		switch (stage)
-		{
-			case 0:
-				if (!this.moveToResource.active) this.moveToResource.active = true;
-				break;
-			case 1:
-				if (!this.mineResource.active) this.mineResource.active = true;
-				break;
-			case 2:
-				if (!this.moveToSupplyPoint.active) this.moveToSupplyPoint.active = true;
-				break;
-			case 3:
-				if (!this.accessChest.active) this.accessChest.active = true;
-				break;
-		}
-		*/
+	    switch(stage) {
+            case DEPOSIT_RESOURCES:
+                if (!this.moveToSupplyPoint.active)
+                {
+                    this.moveToSupplyPoint.active = true;
+                    Minecraft.getMinecraft().player.sendChatMessage("Lumberjack: new stage: DEPOSIT_RESOURCES");
+                }
+                break;
+            case GATHER_RESOURCES:
+                if (!this.chopWood.active)
+                {
+                    this.chopWood.active = true;
+                    Minecraft.getMinecraft().player.sendChatMessage("Lumberjack: new stage: GATHER_RESOURCES");
+                }
+                break;
+            case NONE:
+                moveToNextStage();
+                break;
+            case IDLE:
+                moveToNextStage();
+                break;
+            default:
+                moveToNextStage();
+                break;
+        }
 		super.updateAITasks();
 		// This is called every tick and should have the AI switching code (See Lumberjack)
 	}
@@ -78,5 +84,27 @@ public class EntityLumberjack extends EntityWorker implements INpc
     {
         super.readEntityFromNBT(compound);
         this.setAdditionalAItasks();
+    }
+
+    @Override
+    public void moveToNextStage() {
+        switch(stage) {
+            case DEPOSIT_RESOURCES:
+                stage = EnumEntityStage.GATHER_RESOURCES;
+                break;
+            case GATHER_RESOURCES:
+                // TODO: Check if it has enough resources before depositing
+                stage = EnumEntityStage.DEPOSIT_RESOURCES;
+                break;
+            case IDLE:
+                stage = EnumEntityStage.GATHER_RESOURCES;
+                break;
+            case NONE:
+                stage = EnumEntityStage.IDLE;
+                break;
+            default:
+                stage = EnumEntityStage.NONE;
+                break;
+        }
     }
 }
